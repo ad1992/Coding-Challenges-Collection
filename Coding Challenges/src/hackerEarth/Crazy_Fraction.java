679d2ddcd4eaf89725bf0674d556a572cd8614be
Aakansha <monstershome@gmail.com>
2015-06-20 18:30:08 +0530

    Organized folders and added more programs

--- Coding Challenges/src/hackerEarth/Crazy_Fraction.java
+++ Coding Challenges/src/hackerEarth/Crazy_Fraction.java
@@ -1,6 +1,5 @@
 package hackerEarth;
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedReader;import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
